50afcb9dac82853fed9ce6506f4b1e1e61fa63ce
Olakunle <olakunleakinpelu@gmail.com>
2020-02-21 14:05:32 -0500

    initial refactoring

--- _site/settings.py
+++ _site/settings.py
@@ -80,9 +80,16 @@ WSGI_APPLICATION = '_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'kunle',
+        'PASSWORD': 'testing',
+        'HOST': '139.88.179.199',
+    },
+    # 'db_sqlite': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
