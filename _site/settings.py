77dd1cf24cd1210ecdc3362dff8b993fefbe8457
Olakunle <olakunleakinpelu@gmail.com>
2020-02-24 18:56:52 -0500

    more db admin

--- _site/settings.py
+++ _site/settings.py
@@ -93,11 +93,7 @@ DATABASES = {
         'USER': 'kunle',
         'PASSWORD': 'testing',
         'HOST': '139.88.179.199',
-    },
-    # 'db_sqlite': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
+    }
 }
 
 
